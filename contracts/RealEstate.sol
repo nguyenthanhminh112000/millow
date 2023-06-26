dbf967397aa0dab42b19dc2eedaaca7ee4e25844
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2023-06-26 14:14:53 +0700

    feature(ML-3):deploy smartcontract

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -9,13 +9,15 @@ contract RealEstate is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("Real Estate", "REAL") {}
+    constructor() ERC721("Real Estate", "REAL") {}
 
     function mint(string memory tokenURI) public returns (uint256) {
         _tokenIds.increment();
+
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
+
         return newItemId;
     }
 
